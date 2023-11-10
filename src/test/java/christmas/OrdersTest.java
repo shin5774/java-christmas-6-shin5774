eff47fa89375328bf73af9daf115a3ed3a2b6e0b
shin5774 <babo5774@naver.com>
2023-11-10 21:28:10 +0900

    Test: 할인 전 총 주문금액 반환기능 테스트코드 작성
    - 할인 전의 주문메뉴들의 금액 총합을 반환한다.

---
+++ src/test/java/christmas/OrdersTest.java
@@ -0,0 +1,18 @@
+package christmas;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrdersTest {
+    @DisplayName("주문 메뉴들의 가격의 합을 구하는 기능")
+    @Test
+    void 할인전_총_주문_금액_반환() {
+        Map<Menu, Integer> order = new EnumMap<>(Menu.class);
+        Arrays.stream(Menu.values()).forEach(menu -> order.put(menu, 1));
+        Orders orders = Orders.of(order);
+        assertThat(orders.getTotalOrderAmount()).isEqualTo(296500);
+    }
+}
