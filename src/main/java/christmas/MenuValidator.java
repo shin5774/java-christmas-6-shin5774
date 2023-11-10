7b9c41552894ae85b3c35a0af033f9ddefb32515
shin5774 <babo5774@naver.com>
2023-11-11 03:04:36 +0900

    Feat: 메뉴입력값의 수량값 최소량 이하 예외처리 구현
    - 입력한 메뉴의 수량이 주문의 최소수량(1) 미만인경우 Exception을 발생시킴.

--- src/main/java/christmas/MenuValidator.java
+++ src/main/java/christmas/MenuValidator.java
@@ -1,12 +1,21 @@
 package christmas;
 
 public class MenuValidator {
+    private static final int MINIMUM_ORDER_AMOUNT = 1;
+
     public static void validate(MenuAndAmount menuAndAmount) {
         amountNumberValidate(menuAndAmount.amount());
+        amountMinimunValidate(Integer.parseInt(menuAndAmount.amount()));
     }
 
     private static void amountNumberValidate(String amount) {
         if (Validator.isNotNumber(amount)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void amountMinimunValidate(int amount) {
+        if (amount < MINIMUM_ORDER_AMOUNT) {
             throw new IllegalArgumentException();
         }
     }
