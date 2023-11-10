3c3a52a6f6ee14de678a62850a291634d88ca130
shin5774 <babo5774@naver.com>
2023-11-10 15:59:34 +0900

    Feat: 평일/주말 할인 적용날짜 확인 기능 구현
    - 주말의 경우 일주일 단위인 7로 나눈 나머지가 1 혹은 2인 날들이다.
    - 이를 이용해 평일과 주말을 구분한다.

--- src/main/java/christmas/Promotion.java
+++ src/main/java/christmas/Promotion.java
@@ -5,4 +5,8 @@ public class Promotion {
     public boolean isChristMasDiscount(int day) {
         return day <= 25;
     }
+
+    public boolean isWeekDayDiscount(int day) {
+        return !(day % 7 == 1 || day % 7 == 2);
+    }
 }
