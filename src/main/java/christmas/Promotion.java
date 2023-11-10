addfbb5a1c8c517709882f5c296d3f13c10e4f54
shin5774 <babo5774@naver.com>
2023-11-10 19:10:22 +0900

    Feat: 프로모션별 할인 메뉴수 반환기능 구현
    - 주문 메뉴를 담고있는 EnumMap을 인자로 받아서 메뉴수를 반환
    - 각 프로모션별로 적용되는 메뉴 그룹들을 Enum의 값으로 가짐.
    - 해당 값을 통해 stream.filter를 사용해 분류.

--- src/main/java/christmas/Promotion.java
+++ src/main/java/christmas/Promotion.java
@@ -2,24 +2,28 @@ package christmas;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 public enum Promotion {
-    CHRISTMAS_D_DAY(day -> 1000 + (day - 1) * 100, day -> day <= 25, always -> 1),
-    WEEKDAY(always -> 2023, day -> !(day % 7 == 1 || day % 7 == 2), amount -> amount),
-    WEEKEND(always -> 2023, day -> day % 7 == 1 || day % 7 == 2, amount -> amount),
-    SPECIAL(always -> 1000, day -> List.of(3, 10, 17, 24, 25, 31).contains(day), always -> 1);
+    CHRISTMAS_D_DAY(day -> 1000 + (day - 1) * 100, day -> day <= 25, always -> 1, MenuGroup.NONE),
+    WEEKDAY(always -> 2023, day -> !(day % 7 == 1 || day % 7 == 2), amount -> amount, MenuGroup.DESSERT),
+    WEEKEND(always -> 2023, day -> day % 7 == 1 || day % 7 == 2, amount -> amount, MenuGroup.MAIN),
+    SPECIAL(always -> 1000, day -> List.of(3, 10, 17, 24, 25, 31).contains(day), always -> 1, MenuGroup.NONE);
 
 
     private final UnaryOperator<Integer> discountAmount;
     private final Predicate<Integer> applicable;
     private final UnaryOperator<Integer> menuAmount;
+    private final MenuGroup discountMenuGroup;
 
-    Promotion(UnaryOperator<Integer> discountAmount, Predicate<Integer> applicable, UnaryOperator<Integer> menuAmount) {
+    Promotion(UnaryOperator<Integer> discountAmount, Predicate<Integer> applicable, UnaryOperator<Integer> menuAmount,
+              MenuGroup discountMenuGroup) {
         this.discountAmount = discountAmount;
         this.applicable = applicable;
         this.menuAmount = menuAmount;
+        this.discountMenuGroup = discountMenuGroup;
     }
 
     public static List<Promotion> findPromotions(int day) {
@@ -30,5 +34,11 @@ public enum Promotion {
 
     public int getDiscountAmount(int day, int menuAmount) {
         return discountAmount.apply(day) * this.menuAmount.apply(menuAmount);
+    }
+
+    public int getDiscountMenuAmount(Map<Menu, Integer> order) {
+        return (int) order.keySet().stream()
+                .filter(menu -> MenuGroup.findGroup(menu) == discountMenuGroup)
+                .count();
     }
 }
