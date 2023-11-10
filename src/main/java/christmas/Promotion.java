16eeb6dd4e3dbb0139df1179893237dc6bc15fb5
shin5774 <babo5774@naver.com>
2023-11-10 16:09:24 +0900

    Feat: 특별할인 적용날짜 확인 기능 구현
    - 특별할인일을 담은 리스트에 인자로 들어온 날짜가 존재하는지 확인한다.

--- src/main/java/christmas/Promotion.java
+++ src/main/java/christmas/Promotion.java
@@ -1,5 +1,7 @@
 package christmas;
 
+import java.util.List;
+
 public class Promotion {
 
     public boolean isChristMasDiscount(int day) {
@@ -8,5 +10,9 @@ public class Promotion {
 
     public boolean isWeekDayDiscount(int day) {
         return !(day % 7 == 1 || day % 7 == 2);
+    }
+
+    public boolean isSpecialDiscount(int day) {
+        return List.of(3, 10, 17, 24, 25, 31).contains(day);
     }
 }
