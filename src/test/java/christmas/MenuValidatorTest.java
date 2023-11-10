7923e548e53fc7d3f00ae84a7d8ebac4c8ab2767
shin5774 <babo5774@naver.com>
2023-11-11 02:46:43 +0900

    Test: 메뉴입력값의 수량값 문자입력 예외처리 테스트코드 작성
    - 메뉴를 입력하는과정에서 수량을 입력해야하는 곳에 문자를 입력한경우의 예외처리 기능.

---
+++ src/test/java/christmas/MenuValidatorTest.java
@@ -0,0 +1,25 @@
+package christmas;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("메뉴에 대한 검증 기능")
+public class MenuValidatorTest {
+
+    @Nested
+    @DisplayName("메뉴 수량의 예외처리")
+    class AmountException {
+        @DisplayName("숫자가 아닌 수량입력값")
+        @ParameterizedTest
+        @ValueSource(strings = {"a-a", "a-", "1-a"})
+        void 문자입력(String requestMenu) {
+            MenuAndAmount menuAndAmount = Parser.parseMenuAndAmount(requestMenu);
+            assertThatThrownBy(() -> MenuValidator.validate(menuAndAmount)).isInstanceOf(
+                    IllegalArgumentException.class);
+        }
+    }
+}
