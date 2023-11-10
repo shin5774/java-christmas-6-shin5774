fc41f67e4c0e6e222f91844e1cd034974974623a
shin5774 <babo5774@naver.com>
2023-11-11 03:39:25 +0900

    Feat: 메뉴 개수 상한초과 예외처리 구현
    - 주문 메뉴들의 개수의 합이 상한선(20개)을 초과한 경우의 Exception을 발생시킴.

--- src/main/java/christmas/MenusValidator.java
+++ src/main/java/christmas/MenusValidator.java
@@ -3,8 +3,11 @@ package christmas;
 import java.util.List;
 
 public class MenusValidator {
+    private static final int AMOUNT_UPPER_LIMIT = 20;
+
     public static void validate(List<MenuAndAmount> menus) {
         duplicateMenuValidate(menus);
+        exceededLimitValidate(menus);
     }
 
     private static void duplicateMenuValidate(List<MenuAndAmount> menus) {
@@ -13,6 +16,17 @@ public class MenusValidator {
                 .distinct().count();
 
         if (menus.size() != uniqueCount) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void exceededLimitValidate(List<MenuAndAmount> menus) {
+        int totalAmount = menus.stream()
+                .map(MenuAndAmount::amount)
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        if (totalAmount > AMOUNT_UPPER_LIMIT) {
             throw new IllegalArgumentException();
         }
     }
