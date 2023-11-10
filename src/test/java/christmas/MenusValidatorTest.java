e67fbf0ab5b9fcc0a9bdc4a9a629fb649796b17d
shin5774 <babo5774@naver.com>
2023-11-11 03:34:42 +0900

    Test: 메뉴 개수 상한초과 예외처리 테스트코드 작성
    - 주문 메뉴들의 개수의 합이 상한선을 초과한 경우의 예외처리.

--- src/test/java/christmas/MenusValidatorTest.java
+++ src/test/java/christmas/MenusValidatorTest.java
@@ -23,6 +23,17 @@ public class MenusValidatorTest {
                 MenusValidator.validate(menus);
             }).isInstanceOf(IllegalArgumentException.class);
         }
+
+        @DisplayName("메뉴의 개수 합이 20개 초과입력 예외처리")
+        @Test
+        void 메뉴개수_상한초과() {
+            List<MenuAndAmount> menus = List.of(new MenuAndAmount("양송이수프", "4"), new MenuAndAmount("초코케이크", "6"),
+                    new MenuAndAmount("아이스크림", "3"), new MenuAndAmount("바비큐립", "4"), new MenuAndAmount("레드와인", "5"));
+
+            assertThatThrownBy(() -> {
+                MenusValidator.validate(menus);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
 
