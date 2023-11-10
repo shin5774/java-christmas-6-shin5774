4db3d2c3693e03a0e93b0a6bc08229f4d7225559
shin5774 <babo5774@naver.com>
2023-11-11 03:27:44 +0900

    Test: 메뉴중복 입력 테스트코드 작성
    - 중복된 메뉴가 입력된 경우 예외를 발생시킴.

---
+++ src/test/java/christmas/MenusValidatorTest.java
@@ -0,0 +1,29 @@
+package christmas;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class MenusValidatorTest {
+
+
+    @DisplayName("메뉴들을 예외처리")
+    @Nested
+    class MenusException {
+        @DisplayName("메뉴의 중복 입력 예외처리")
+        @Test
+        void 메뉴중복입력() {
+            List<MenuAndAmount> menus = List.of(new MenuAndAmount("양송이수프", "2"), new MenuAndAmount("초코케이크", "1"),
+                    new MenuAndAmount("아이스크림", "3"), new MenuAndAmount("바비큐립", "1"), new MenuAndAmount("양송이수프", "3"));
+
+            assertThatThrownBy(() -> {
+                MenusValidator.validate(menus);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+
+}
