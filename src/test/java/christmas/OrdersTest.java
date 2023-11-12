75e9d7c3a0f86c15aa61c5de2796891c987f77e7
shin5774 <babo5774@naver.com>
2023-11-13 04:13:42 +0900

    Test: 주문내역 반환기능 테스트코드 작성
    - 주문내역을 메뉴의 이름과 개수의 형식으로 반환해주는 기능

--- src/test/java/christmas/OrdersTest.java
+++ src/test/java/christmas/OrdersTest.java
@@ -4,17 +4,38 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.Arrays;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OrdersTest {
+    private static Orders orders;
+
+    @BeforeAll
+    static void 초기설정() {
+        Map<Menu, Integer> order = new EnumMap<>(Menu.class);
+        Arrays.stream(Menu.values())
+                .filter(menu -> !menu.equals(Menu.NONE))
+                .forEach(menu -> order.put(menu, 1));
+        orders = Orders.of(order);
+    }
+
     @DisplayName("주문 메뉴들의 가격의 합을 구하는 기능")
     @Test
     void 할인전_총_주문_금액_반환() {
-        Map<Menu, Integer> order = new EnumMap<>(Menu.class);
-        Arrays.stream(Menu.values()).forEach(menu -> order.put(menu, 1));
-        Orders orders = Orders.of(order);
         assertThat(orders.getTotalOrderAmount()).isEqualTo(296500);
+    }
+
+    @DisplayName("주문 메뉴의 이름과 개수를 반환해주는 기능")
+    @Test
+    void 주문_반환() {
+        List<String> menuNames = Arrays.stream(Menu.values())
+                .map(Menu::getName)
+                .toList();
+
+        orders.getOrderDetails().keySet()
+                .forEach(menu -> assertThat(menuNames.contains(menu)).isTrue());
     }
 }
