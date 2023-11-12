70492fc502c4a939c1e240371ec0048881526179
shin5774 <babo5774@naver.com>
2023-11-13 03:30:50 +0900

    Test: 뱃지 반환 테스트코드 작성
    - 할인금액을 넣어주면 그에 맞는 뱃지를 반환해주는 기능.

---
+++ src/test/java/christmas/BadgeTest.java
@@ -0,0 +1,18 @@
+package christmas;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class BadgeTest {
+    @DisplayName("뱃지 반환 기능")
+    @ParameterizedTest
+    @CsvSource(value = {"3000:없음", "7000:별", "13000:트리", "25000:산타"}, delimiter = ':')
+    void 뱃지_반환(int benefitAmount, String expected) {
+        Badge badge = Badge.getBadge(benefitAmount);
+
+        assertThat(badge.toString()).isEqualTo(expected);
+    }
+}
