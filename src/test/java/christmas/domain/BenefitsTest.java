d6d50b6af52c3c2b591788dcb03d80c283cc6ad8
shin5774 <babo5774@naver.com>
2023-11-15 21:33:23 +0900

    Test: Benefits 기능별 테스트코드 작성
    - Benefits 클래스 내부에있는 메서드들의 기능을 검증하는 테스트코드를 작성.

--- src/test/java/christmas/domain/BenefitsTest.java
+++ src/test/java/christmas/domain/BenefitsTest.java
@@ -2,7 +2,6 @@ package christmas.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import christmas.domain.Benefits;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
@@ -27,5 +26,24 @@ public class BenefitsTest {
     @Test
     void 총_할인금액() {
         assertThat(benefits.getTotalBenefitAmount()).isEqualTo(-15);
+    }
+
+    @DisplayName("증정 이벤트 혜택 확인_존재")
+    @Test
+    void 증정_이벤트_존재() {
+        assertThat(benefits.hasGiveawayEvent()).isTrue();
+    }
+
+    @DisplayName("증정 이벤트 혜택 확인_미존재")
+    @Test
+    void 증정_이벤트_미존재() {
+        Map<String, Integer> benefitsData = new HashMap<>();
+        benefitsData.put("크리스마스 디데이 할인", -1);
+        benefitsData.put("평일 할인", -2);
+        benefitsData.put("주말 할인", -3);
+        benefitsData.put("특별 할인", -4);
+        Benefits nonBenefits = Benefits.from(benefitsData);
+        
+        assertThat(nonBenefits.hasGiveawayEvent()).isFalse();
     }
 }
